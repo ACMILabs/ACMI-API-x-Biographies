e75dff2c554a7501ec7b5cfa57ce0e5ba3677a12
Simon Loffler <simon.loffler@acmi.net.au>
2021-10-25 13:36:02 +1030

    Added a Python example of getting biography extracts from Wikipedia based on search results from the ACMI Public API

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# ACMI-API-x-Biographies
-ACMI Public API example matching internet biographies to Work creators
+# ACMI-API-x-Biographie
+
+ACMI Public API example matching internet biographies to Work creators.
+
+## Wikipedia biography data
+
+An example of matching Wikipedia biography data to ACMI collection creators.
+
+### Python
+
+Run `python3 acmi_api_biographies.py` to first search the ACMI Works API for creators named `Simon`, and then search the Wikipedia API to get an extract of the biography of that person.
+
+Example output:
+
+```bash
+$ python3 acmi_api_biographies.py
+
+Found 14 ACMI creators with simon in their name: {'Jamil Simon', 'Phil Simon', 'Jamil Simon Productions', 'Simon Trevor', 'Simon Wincer', 'Simon Price', 'Simon Bruty', 'Simon Penny', 'Simon McIntyre', 'Simon Moore', 'Simon West', 'Simon Bejer', 'Simon Terrill', 'Simon Maidment'}
+
+Sorry, no data from Wikipedia for: Jamil Simon
+
+Wikipedia data for Phil Simon: https://en.wikipedia.org/wiki/Phil_Simon
+Title: Phil Simon
+Extract: Phil Simon (born ca. 1972) is an American speaker, professor, and author. He writes about management, technology, disruption, communication, and analytics.
+
+...
+```
+
+### Jupyter Notebook
+
+TODO...
---
+++ acmi_api_biographies.py
@@ -0,0 +1,71 @@
+"""
+Use the ACMI Public API to match internet biographies to ACMI collection Works.
+"""
+
+import requests
+
+# Search for ACMI creators with 'Simon' in their name
+ACMI_SEARCH_QUERY = 'Simon'.lower()
+creators = set()
+
+if ACMI_SEARCH_QUERY:
+    search_response = requests.get(
+        'https://api.acmi.net.au/search/',
+        params={
+            'query': ACMI_SEARCH_QUERY,
+            'field': 'creators_primary.name',
+        },
+    ).json()
+
+    for result in search_response['results']:
+        for creator in result.get('creators_primary'):
+            if ACMI_SEARCH_QUERY in creator.get('name').lower():
+                creators.add(creator.get('name'))
+
+    print(
+        f'Found {len(creators)} ACMI creators with {ACMI_SEARCH_QUERY} '
+        f'in their name: {creators}'
+    )
+else:
+    # Let's search for all directors instead
+    search_response = requests.get(
+        'https://api.acmi.net.au/search/',
+        params={
+            'query': 'director',
+            'field': 'creators_primary.role',
+        },
+    ).json()
+
+    for result in search_response['results']:
+        for creator in result.get('creators_primary'):
+            if 'director' in creator.get('role').lower():
+                creators.add(creator.get('name'))
+
+    print(f'Found {len(creators)} ACMI creators who are directors: {creators}')
+
+# Search Wikipedia for information on each person
+for creator in creators:
+    wikipedia_search_response = requests.get(
+        'https://en.wikipedia.org/w/api.php',
+        params={
+            'action': 'query',
+            'format': 'json',
+            'titles': creator,
+            'prop': 'info|extracts',
+            'exintro': True,
+            'explaintext': True,
+            'inprop': 'url',
+        },
+    ).json()
+    try:
+        wikipedia_page = next(
+            iter(wikipedia_search_response['query']['pages'].values()),
+        )
+        print(
+            f'Wikipedia data for {creator}: {wikipedia_page["fullurl"]}\n'
+            f'Title: {wikipedia_page["title"]}\n'
+            f'Extract: {wikipedia_page["extract"]}'
+        )
+    except KeyError:
+        print(f'Sorry, no data from Wikipedia for: {creator}')
+    print('')
